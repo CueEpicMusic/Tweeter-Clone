7d284bddab3ba5cfd275f6ae6e5f74ee74e9d10d
CueEpicMusic <chiatunglo@hotmail.com>
2022-07-13 19:46:28 +0000

    add dynamic tweet function to client.js and ssome styling changes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,72 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    createTweetElement(tweet)
+  }
+}
+
+const createTweetElement = function (data) {
+  let $tweet = `<article>
+        <header>
+          <div class="tweeter-name">
+            <img src="${data.user.avatars}">
+            ${data.user.name}
+          </div>
+          <div class="tweeter-handle">
+            <strong>${data.user.handle}</strong>
+          </div>
+        </header>
+        <div class="tweeter-tweet">
+          <span>${data.content.text}</span>
+        </div>
+        <footer>
+          <span>${data.created_at}</span>
+          <ul>
+            <li>
+              <i class="fa-solid fa-flag"></i>
+            </li>
+            <li>
+              <i class="fa-solid fa-retweet"></i>
+            </li>
+            <li>
+              <i class="fa-solid fa-heart"></i>
+            </li>
+          </ul>
+        </footer>
+      </article>`;
+  return $(".main-tweets").append($tweet);
+};
+
+renderTweets(data);
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,11 @@
-$(document).ready(function() {
-  $("#tweet-text").on("input", function (event) {
-    let currentLength = $(this).val().length
-    let output = $(".main-tweets output")
-    output.val(140 - currentLength)
+$("#tweet-text").on("input", function (event) {
+  let currentLength = $(this).val().length
+  let output = $(".main-tweets output")
+  output.val(140 - currentLength)
 
-    if (currentLength > 140) {
-      output.addClass("counter-red")
-    } else {
-      output.removeClass("counter-red")
-    }
-  })
-});
+  if (currentLength > 140) {
+    output.addClass("counter-red")
+  } else {
+    output.removeClass("counter-red")
+  }
+})
